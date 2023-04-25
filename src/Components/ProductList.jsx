aa961299b84eda9967bee841d862738973ef7a9e
Franck <gwadafrank@gmail.com>
2023-04-25 14:20:57 +0300

    Add the nested components

---
+++ src/Components/ProductList.jsx
@@ -0,0 +1,11 @@
+import ProductItem from "./ProductItem";
+
+function ProductList() {
+    return (
+        <>
+            <h2>ProductList</h2>
+            <ProductItem />
+        </>);
+}
+
+export default ProductList;
